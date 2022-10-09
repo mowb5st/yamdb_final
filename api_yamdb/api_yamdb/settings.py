1b27ae9c97a6d952040870f67686d49a0143fc8c
mowb <mowbnew@gmail.com>
2022-10-09 16:08:31 +0700

    pre-review commit

--- api_yamdb/api_yamdb/settings.py
+++ api_yamdb/api_yamdb/settings.py
@@ -61,7 +61,7 @@ DATABASES = {
         'NAME': os.getenv('DB_NAME', default='postgres'),
         'USER': os.getenv('POSTGRES_USER', default='postgres'),
         'PASSWORD': os.getenv('POSTGRES_PASSWORD', default='12345'),
-        'HOST': os.getenv('DB_HOST', default='localhost:127.0.0.1'),
+        'HOST': os.getenv('DB_HOST', default='db'),
         'PORT': os.getenv('DB_PORT', default='8000')
     }
 }
